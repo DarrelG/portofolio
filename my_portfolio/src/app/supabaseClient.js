7397377168dbc135a15360eded9d32bd7daefa57
DarrelG <gautanadarrel06@gmail.com>
2025-07-22 08:32:00 +0700

    Hide Secret Key

--- my_portfolio/src/app/supabaseClient.js
+++ my_portfolio/src/app/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://kqpgdwowjsvnsnehqqui.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtxcGdkd293anN2bnNuZWhxcXVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMwNzk4NTQsImV4cCI6MjA2ODY1NTg1NH0.QucGW-ecV3i5oS_zERoXGF8s3jWglpX7Y4RFGjh2SRY';
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseAnonKey = process.env.SUPABASE_ANONKEY;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
