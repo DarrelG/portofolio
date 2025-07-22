36899f791a3f099e053a7dfa4aa5fe5a2a58a855
DarrelG <gautanadarrel06@gmail.com>
2025-07-22 08:58:13 +0700

    fix supabase url error

--- my_portfolio/src/app/supabaseClient.js
+++ my_portfolio/src/app/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANONKEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANONKEY;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
